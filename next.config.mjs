a6c227e320a0bd493cebd3e5d81dbf291ea9510a
SumanthPal <sumanth@pallamreddy.com>
2025-04-28 11:05:12 -0700

    img optimization

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: [
+        'scontent-lax3-1.cdninstagram.com',
+        'scontent-lax3-2.cdninstagram.com',
+        'scontent-iad3-1.cdninstagram.com',
+        'scontent-iad3-2.cdninstagram.com',
+        'scontent-iad3-2.cdninstagram.com'
+      ],
+    },
+  };
+
+  export default nextConfig;
