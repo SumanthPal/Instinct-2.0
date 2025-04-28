a10d09b0a4e0a5f174b6569e4b5e644e29d25171
SumanthPal <sumanth@pallamreddy.com>
2025-04-28 00:16:25 -0700

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  unoptimized: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.cdninstagram.com',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+  
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'scontent-lax3-1.cdninstagram.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'scontent-lax3-2.cdninstagram.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
