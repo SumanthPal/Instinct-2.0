57e158481b3ca307cd9902cc950b8ac8cfd3dfe9
SumanthPal <sumanth@pallamreddy.com>
2025-04-28 16:39:14 -0700

    img optimization

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'scontent-lax3-1.cdninstagram.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'scontent-lax3-2.cdninstagram.com',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com', // Match ALL Instagram CDN subdomains
+      },
+    ],
+  },
+};
+
+export default nextConfig;
