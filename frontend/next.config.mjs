15f8d72acd0b157891aa150e0b479be8ed681f9c
SumanthPal <sumanth@pallamreddy.com>
2025-09-10 20:55:06 -0700

    update for pwa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,24 @@
+const withPWA = require("next-pwa")({
+	dest: "public",
+	disable: process.env.NODE_ENV === "development",
+	register: true,
+	skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'levelicytjtkbdvbflzv.supabase.co',
-      'instinctucistorage.blob.core.windows.net',
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.cdninstagram.com', // ✅ Correct wildcard format
-      },
-    ],
-  },
+	images: {
+		domains: [
+			"levelicytjtkbdvbflzv.supabase.co",
+			"instinctucistorage.blob.core.windows.net",
+		],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "*.cdninstagram.com", // ✅ Correct wildcard format
+			},
+		],
+	},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
