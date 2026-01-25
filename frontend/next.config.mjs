62375eed8bbdf7cabc63f12a1c274cf230f21d51
SumanthPal <sumanth@pallamreddy.com>
2026-01-24 17:48:02 -0800

    fix to turbopack version

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,26 +1,27 @@
 import nextPWA from "next-pwa";
 
 const withPWA = nextPWA({
-	dest: "public",
-	disable: process.env.NODE_ENV === "development",
-	register: true,
-	skipWaiting: true,
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: [
-			"levelicytjtkbdvbflzv.supabase.co",
-			"instinctucistorage.blob.core.windows.net",
-		],
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "*.cdninstagram.com", // ✅ Correct wildcard format
-			},
-		],
-	},
+  turbopack: {},
+  images: {
+    domains: [
+      "levelicytjtkbdvbflzv.supabase.co",
+      "instinctucistorage.blob.core.windows.net",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.cdninstagram.com", // ✅ Correct wildcard format
+      },
+    ],
+  },
 };
 
 export default withPWA(nextConfig);
